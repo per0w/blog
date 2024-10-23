e6968812caf032c69873da022fc11bfb6d95a258
Vladimir Perov <per0w@yandex.ru>
2024-10-23 19:15:11 +0400

    feat: added github workflows

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 import createMDX from "@next/mdx";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/blog/' : '',
+  basePath: isProd ? '/blog' : '',
+  output: isProd ? 'export' : undefined,
 };
 
 const withMDX = createMDX({});
