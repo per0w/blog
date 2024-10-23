75cb7cd562fb54c0e9f44d94397fa06daf500a10
Vladimir Perov <per0w@yandex.ru>
2024-10-23 19:18:24 +0400

    feat: added github workflows

--- next.config.mjs
+++ next.config.mjs
@@ -5,9 +5,6 @@ const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
   assetPrefix: isProd ? '/blog/' : '',
   basePath: isProd ? '/blog' : '',
   output: isProd ? 'export' : undefined,
